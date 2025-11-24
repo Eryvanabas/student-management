97c120710041ec50bd24e6892990060b3256b023
Eryvanabas <116214549+Eryvanabas@users.noreply.github.com>
2025-11-24 08:31:32 -0800

    first upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Student Mangement System</title>
+</head>
+<body>
+  
+<style>
+  .hover-link:hover {
+    color: success; /* blue hover */
+  }
+</style>
+
+    <nav class="navbar navbar-expand-lg" style="background-color: #111;">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link nav-link text-white hover-link"  href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link nav-link text-white" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link nav-link text-white" href="#">Admission</a>
+        </li>
+    <button class="btn btn-outline-success" class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span> Log in</span>
+    </button>
+      </ul>
+      
+      <span class="navbar-text badge text-bg-success">
+        E-School                                        
+      </span>
+    </div>
+  </div>
+</nav>
+    <div>
+      <label> We teach Student with care</label>
+        <img  class="section1" src="6.jpg" alt="My Image">
+    </div>
+</body>
+</html>
