e46e5d21a3b8000257452bc2a37f5ddab40fc05e
Eryvanabas <116214549+Eryvanabas@users.noreply.github.com>
2025-11-27 07:00:56 -0800

    css

--- index.php
+++ index.php
@@ -93,6 +93,30 @@ Our management platform helps you stay organized and informed.
         </div>
       </div>
     </div>
+      <section class="text-center">
+      <h1>Admission Form</h1>
+    </section>
+    
+    <form class="container my-5">
+     
+  <div class="mb-3">
+    <label  class="form-label">Name</label>
+    <input type="text" class="form-control" aria-describedby="emailHelp"></div>
+    <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="form-floating">
+  <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+  <label for="floatingTextarea2">Comments</label>
+</div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
   
 </body>
 </html>
