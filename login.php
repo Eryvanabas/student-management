f7092ad86d023425ae3096be98d15de14ffb392d
Eryvanabas <116214549+Eryvanabas@users.noreply.github.com>
2025-12-02 13:20:16 -0800

    log

--- login.php
+++ login.php
@@ -9,18 +9,22 @@
     <title>Log in form</title>
 </head>
 <body>
-    <h1>log in form</h1>
-    <form>
+    
+     <section class="bg-section">
+      <h1 class="fw-medium text-center">log in form</h1>
+   
+    
+    <form class="container my-10">
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">User name</label>
-    <input type="text" name="text"  class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <label class="form-label">User name</label>
+    <input type="text" name="text" >
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+    <input type="password" name="password"  id="exampleInputPassword1">
   </div>
   <button type="submit"  name="submit"class="btn btn-primary">Submit</button>
 </form>
+ </section>
 </body>
 </html>
